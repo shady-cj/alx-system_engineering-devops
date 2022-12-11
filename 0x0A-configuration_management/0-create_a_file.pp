15ff9de18466da228ec25d077e024e7ec02759f2
peter <petersp2000@gmail.com>
2022-12-11 09:09:31 +0100

    First Puppet task

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,3 +1,8 @@
+# Using puppet to create a file /tmp/school
+# The file owner and group should be www-data
+# The file permission should be 0744
+# The file content should be 'I love Puppet'
+
 file { 'school':
 ensure  => present,
 path    => '/tmp/school',
