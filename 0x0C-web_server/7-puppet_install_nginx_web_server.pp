b6e3d74053c78731256f6ebe5d3db1e9a7194a0f
shady-cj <petersp2000@gmail.com>
2022-12-14 15:49:57 +0100

    commit

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,44 @@
+# using puppet to install and configure nginx
+
+
+package { 'nginx':
+	ensure => installed,
+	before => Service['nginx']
+}
+
+service { 'nginx':
+	ensure => running,
+	require => Package['nginx'],
+	subscribe => File['/etc/nginx/sites-available/default']
+}
+
+
+File { '/var/www/html/index.html':
+	ensure => file,
+	content => 'Hello World!'
+}
+
+File { '/var/www/html/404.html':
+	ensure => file,
+	content => "Ceci n'est pas une page"
+}
+
+@content = @(CONTENT)
+	server {
+        	listen 80 default_server;
+        	listen [::]:80 default_server;
+        	root /var/www/html;
+        	index index.html index.htm index.nginx-debian.html;
+
+        	location /redirect_me {
+                	return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        	}
+
+        	error_page 404  /404.html;
+
+	}
+
+File { '/etc/nginx/sites-available/default':
+	ensure => present,
+	content => $content
+}
