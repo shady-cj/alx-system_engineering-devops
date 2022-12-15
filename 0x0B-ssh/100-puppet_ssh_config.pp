94fcb15d16256269c09b87b7317e1c732142a171
peter <petersp2000@gmail.com>
2022-12-15 05:03:53 +0100

    puppet edit file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,19 +1,5 @@
 # A puppet manifest to make SSH client configuration in order to use the private key ~/.ssh/school
 # A puppet manifest to make SSH client configuration in order to refuse to authenticate using a password
-include stdlib
-file_line { 'passwordauth':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
-  match  => '.*PasswordAuthentication.*'
-}
 
-
-file_line { 'IdentityFile':
-  ensure   => present,
-  path     => '/etc/ssh/ssh_config',
-  after    => 'StrictHostKeyChecking',
-  line     => '    IdentityFile ~/.ssh/school',
-  match    => '.*IdentityFile.*',
-  multiple => true
-}
+exec { "/bin/sed -i '0,/.*PasswordAuthentication.*/{s/.*PasswordAuthentication.*/    PasswordAuthentication no/}' /etc/ssh/ssh_config": }
+exec { "/bin/sed -i '0,/.*IdentityFile.*/{s/.*IdentityFile.*/    IdentityFile \~\/\.ssh\/school/}' /etc/ssh/ssh_config": }
