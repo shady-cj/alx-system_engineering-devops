f09995d14687d127a632dda0c46095a74750c0f7
peter <petersp2000@gmail.com>
2023-02-10 07:32:24 +0100

    reloading gunicorn with no downtime

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,10 @@
-# Configuration to improve the request handling of nginx
+# configuration to improve the request handling of nginx
 
-exec { "/bin/sed -i 's/ULIMIT.*/ULIMIT=\"-n 1024\"/g' /etc/default/nginx": }
-exec { '/usr/sbin/service nginx restart': }
+exec { 'ulimit':
+        command => "/bin/sed -i 's/ULIMIT.*/ULIMIT=\"-n 1024\"/g' /etc/default/nginx"
+}
+
+service { 'nginx':
+        ensure    => running,
+        subscribe => Exec['ulimit']
+}
