1827d6f7c9461bbd9db513e39f50977a6f6c5fee
peter <petersp2000@gmail.com>
2023-02-10 07:43:49 +0100

    increasing users ulimit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,5 @@
 # increasing the limit of files that can be opened for a restricted user
 
-exec { "sed -i '/holberton.*/d' /etc/security/limits.conf": }
+exec { "sed -i '/holberton hard/s/5/10000/g' /etc/security/limits.conf": }
+
+exec { "sed -i '/holberton soft/s/4/9000/g' /etc/security/limits.conf": }
